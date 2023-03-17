336b2604f3b07cb747caa6ccea6997651b035109
zivbabs <117026503+zivbabs@users.noreply.github.com>
2023-03-17 16:07:17 +0100

    update working

--- src/main/java/dk/webshopexercize/controller/MyController.java
+++ src/main/java/dk/webshopexercize/controller/MyController.java
@@ -1,4 +1,20 @@
 package dk.webshopexercize.controller;
 
+import dk.webshopexercize.repository.ProductRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class MyController {
+    ProductRepository productRepository;
+    public MyController(ProductRepository productRepository){
+        this.productRepository = productRepository;
+    }
+
+    @GetMapping("/")
+    public String index(Model model){
+        model.addAttribute("products", productRepository.getAll());
+        return "index";
+    }
 }
